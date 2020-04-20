61f7e99fa024ef051b19d8f96932d3a01ce2beab
QiuJJ <qiujj@jpush.cn>
2020-04-20 14:23:35 +0800

    废弃，换别的框架，最后一次提交

--- Djdemo/urls.py
+++ Djdemo/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from Test import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello$', views.hello)
+    url(r'^hello$', views.hello),
+    url(r'^test$', views.test)
 ]
--- Test/views.py
+++ Test/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-
 # Create your views here.
 
 from django.http import HttpResponse         #需要导入HttpResponse模块
 
 def hello(request):                          #request参数必须有，名字类似self的默认规则，可以修改，它封装了用户请求的所有内容
     return HttpResponse("Hello world ! ")
+
+def test(request):                          #request参数必须有，名字类似self的默认规则，可以修改，它封装了用户请求的所有内容
+    return HttpResponse("test ! ")
+
+
+def process_parameters(body):
+     try:
+         body_dic = {}
+         key_values = str(body).split("&")
+         for key_value in key_values:
+             body_dic[key_value.split("=")[0]] = key_value.split("=")[1]
+         return body_dic
+     except Exception as e:
+         return e
